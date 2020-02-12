3c550fd6aa70c1e07d484e642de379f29563922a
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-12 13:13:28 -0500

    Feat - add Form component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
+import Form from './components/Form';
 
 function App() {
+
+  const [search, setSearch] = useState({
+      city: '',
+      country: ''
+  });
+
+  const [ consult, setConsult ] = useState(true);
+
+  const { city, country } = search;
+  
+  useEffect(() => {
+    const fetchAPI = async () => {
+      //const api = fetch(`https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}`);
+    }
+    fetchAPI();
+  }, [consult]);
+
   return (
     <Fragment>
       <Header 
         title="Weather React"
       />
+      <div className="container-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Form
+                search={search}
+                setSearch={setSearch}
+                setConsult={setConsult}
+              />
+            </div>
+            <div className="col m6 s12">
+              2
+            </div>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const Form = ({ search, setSearch, setConsult }) => {
+
+    const { city, country } = search;
+
+    const [error, setError] = useState(false);
+
+    const handleChange = e => {
+        setSearch({
+            ...search,
+            [e.target.name] : e.target.value
+        });
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if(city.trim() === '' || country.trim() === ''){
+            setError(true);
+            return;
+        }
+        
+        setError(false);
+
+        setConsult(true);
+
+
+    } 
+
+    return ( 
+        <form
+            onSubmit={handleSubmit}
+            >
+            { error ? <p className="red darken-4 error">All fields are required</p> : null }
+            <div className="input-field col s12">
+                <input 
+                    type="text" 
+                    name="city" 
+                    id="city"
+                    value={city}
+                    onChange={handleChange}
+                    autoComplete="none"
+                />
+                <label htmlFor="city">City: </label>
+            </div>
+            <div className="input-field col s12">
+                <select
+                    name="country"
+                    id="country"
+                    value={country}
+                    onChange={handleChange}
+                >
+                    <option value="">-- Choose a country --</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
+                </select>
+                <label htmlFor="country">Country: </label>
+            </div>
+            <div className="input-field col s12">
+                <input
+                    type="submit"
+                    value="Search Weather"
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                />
+            </div>
+        </form>
+    );
+}
+
+Form.propTypes = {
+    search: PropTypes.object.isRequired,
+    setSearch: PropTypes.func.isRequired,
+    setConsult: PropTypes.func.isRequired
+}
+ 
+export default Form;
