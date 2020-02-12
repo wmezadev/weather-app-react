51bb79d1d59ff01fbd0345f89548c43a898dfa89
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-12 10:31:17 -0500

    Feat - add style materialize and firts header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-
+import React, { Fragment } from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>
-      Weather React
-    </h1>
+    <Fragment>
+      <Header 
+        title="Weather React"
+      />
+    </Fragment>
   );
 }
 
